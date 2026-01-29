109ca76730ae900e2f63e7217a38db523129503b
Vedant <bhavsarvedant9@gmail.com>
2026-01-29 13:41:26 +0530

    appwrite backend fetch

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+  // console.log(import.meta.env.VITE_APPWRITE_URL);
+  
+  return (
+      <>
+      <h1 className='text-amber-700'>Hello ji </h1>
+    </>
+  )
+}
+
+
+export default App
---
+++ src/appwrite/auth.js
@@ -0,0 +1,53 @@
+import config from "../config/config.js";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+  client = new Client();
+  account;
+  constructor() {
+    this.client
+      .setEndpoint(config.appwriteURL) // Your API Endpoint
+      .setProject(config.appwriteProjectId); // Your project ID
+  }
+  async createAccount({ email, password, name }) {
+    const userAcc=await this.account.create(ID.unique(), email, password);
+    return userAcc
+  }
+  async login({email,password}) {
+    return await this.account.createEmailPasswordSession(email, password);
+    
+  }
+
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      console.log(error)
+      throw error;
+    }
+  }
+
+  async logout() {
+    try {
+      await this.account.deleteSessions()
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+  }
+}
+
+const authService = new AuthService();
+export default AuthService;
+
+// const client = new Client()
+//   .setEndpoint("https://<REGION>.cloud.appwrite.io/v1") // Your API Endpoint
+//   .setProject("<PROJECT_ID>"); // Your project ID
+
+// const account = new Account(client);
+
+// const user = await account.create({
+//   userId: ID.unique(),
+//   email: "email@example.com",
+//   password: "password",
+// });
